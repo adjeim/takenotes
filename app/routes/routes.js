acbc54ed3e48d4caa7434a3adc5acfe1d7ecf040
Mia Adjei <adjei.m@gmail.com>
2017-06-03 18:42:11 -0400

    Update routes to use mongo database.

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,17 @@
 module.exports = function(app, db) {
 	app.post('/notes', (req, res) => {
-		console.log(req.body)
-		res.send('Hello there!')
+		const note = { 
+			text: req.body.body, 
+			title: req.body.title
+		};
+		db.collection('notes').insert(note, (err, result) => {
+			if (err) {
+				res.send({ 'error': 'An error has occurred'});
+			} else {
+				res.send(result.ops[0]);
+			}
+		});
+		// console.log(req.body)
+		// res.send('Hello there!')
 	});
 };
